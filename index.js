9998598e7cb9a1636d2a597fab4548d4dd8490ec
ALI <ahmmsel2@gmail.com>
2022-12-16 05:36:32 +0200

    init

--- index.js
+++ index.js
@@ -6,30 +6,30 @@ for (let i = 0; i < card.length; i++) {
   });
 }
 
-// let imageArr = document.querySelectorAll('.img');
-// let prevX = 0;
-// let prevY = 0;
-// let moveX = 0;
-// let moveY = 0;
+let imageArr = document.querySelectorAll('.img');
+let prevX = 0;
+let prevY = 0;
+let moveX = 0;
+let moveY = 0;
 
-// document.addEventListener('mousemove', (e) => {
-//   effect(e);
-// });
+document.addEventListener('mousemove', (e) => {
+  effect(e);
+});
 
-// function effect(e) {
-//   moveX = e.pageX = prevX;
-//   moveY = e.pageY = prevY;
-//   moveImage(moveX, moveY);
-//   prevX = e.pageX;
-//   prevY = e.pageY;
-//   console.log(e.pageX)
-// }
+function effect(e) {
+  moveX = e.pageX - prevX;
+  moveY = e.pageY - prevY;
+  moveImage(moveX, moveY);
+  prevX = e.pageX;
+  prevY = e.pageY;
+  console.log(e.pageX)
+}
 
-// function moveImage(x, y) {
-//   imageArr.forEach((img) => {
-//     let move = 5 + (Math.random() * 15);
+function moveImage(x, y) {
+  imageArr.forEach((img) => {
+    let move = 5 + (Math.random() * 15);
 
-//     img.style.left = (img.offsetLeft - x / move) + 'px';
-//     img.style.top = (img.offsetTop - y / move) + 'px';
-//   });
-// }
+    img.style.left = (img.offsetLeft - x / move) + 'px';
+    img.style.top = (img.offsetTop - y / move) + 'px';
+  });
+}
