0b7924df53168c27d65ee9a525af250f5e288eb6
ALI <ahmmsel2@gmail.com>
2022-12-16 04:25:43 +0200

    init

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ for (let i = 0; i < card.length; i++) {
     card[i].classList.toggle('flipCard');
   });
 }
+
+// let imageArr = document.querySelectorAll('.img');
+// let prevX = 0;
+// let prevY = 0;
+// let moveX = 0;
+// let moveY = 0;
+
+// document.addEventListener('mousemove', (e) => {
+//   effect(e);
+// });
+
+// function effect(e) {
+//   moveX = e.pageX = prevX;
+//   moveY = e.pageY = prevY;
+//   moveImage(moveX, moveY);
+//   prevX = e.pageX;
+//   prevY = e.pageY;
+//   console.log(e.pageX)
+// }
+
+// function moveImage(x, y) {
+//   imageArr.forEach((img) => {
+//     let move = 5 + (Math.random() * 15);
+
+//     img.style.left = (img.offsetLeft - x / move) + 'px';
+//     img.style.top = (img.offsetTop - y / move) + 'px';
+//   });
+// }
