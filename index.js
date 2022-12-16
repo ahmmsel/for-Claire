189b9a5de057963a9b468c22ffe8d299efc46503
ALI <ahmmsel2@gmail.com>
2022-12-16 04:13:36 +0200

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const card = document.querySelectorAll('.card');
+
+for (let i = 0; i < card.length; i++) {
+  card[i].addEventListener('click', function () {
+    card[i].classList.toggle('flipCard');
+  });
+}
